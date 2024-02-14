a0a0f0ce68966399063332fa755794b6a424b071
ericet <ericetchen@gmail.com>
2024-02-13 22:52:30 -0500

    Refactor the code

--- 4-multiSend.js
+++ 4-multiSend.js
@@ -1,20 +1,16 @@
 import {
     QueryClient, setupDistributionExtension, setupBankExtension, setupStakingExtension, setupTxExtension, setupGovExtension, SigningStargateClient, calculateFee
 } from "@cosmjs/stargate";
-import {
-    PrivateKey,
-    InjectiveDirectEthSecp256k1Wallet
-} from "@injectivelabs/sdk-ts"
 import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
 import fs from "fs";
-import EthermintSigningClient from "./utils/EthermintSigningClient.js";
-import { validateMnemonic } from 'bip39';
-import { coins,Secp256k1HdWallet } from '@cosmjs/launchpad';
+import EthermintSigningClient from "./utils/SigningClient.js";
+import { coins } from '@cosmjs/launchpad';
+import { getSigner } from "./utils/helpers.js";
 
 const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
 const chainsMap = loadJSON('./assets/chains.json');
 
-async function getQueryClient (rpcEndpoint) {
+async function getQueryClient(rpcEndpoint) {
     const tendermint34Client = await Tendermint34Client.connect(rpcEndpoint);
     const queryClient = QueryClient.withExtensions(
         tendermint34Client,
@@ -27,7 +23,7 @@ async function getQueryClient (rpcEndpoint) {
     return queryClient;
 }
 
-async function multiSend (client, from, recipients, amount, chain) {
+async function multiSend(client, from, recipients, amount, chain) {
     let ops = [];
     for (let recipient of recipients) {
         let msg = {
@@ -41,49 +37,17 @@ async function multiSend (client, from, recipients, amount, chain) {
         ops.push(msg);
     }
 
-    let result;
-    if (chain.slip44 && chain.slip44 === 60) {
-        result = await client.signAndBroadcast(from, ops, '', '');
-    } else {
-        let calculatedFee = await estimateFee(client, from, ops, chain);
-        result = await client.signAndBroadcast(from, ops, calculatedFee, '');
-    }
+    let result = await client.signAndBroadcast(from, ops, '', '');
+
     return result;
 
 }
 
-async function estimateFee (client, address, message, chain) {
-    try {
-        let gasLimit = await client.simulate(address, message, '');
-        let calculatedFee = calculateFee(Math.floor(gasLimit * chain.gasLimitRatio), `${chain.gasPrice}${chain.denom}`);
-        return calculatedFee;
-    } catch (err) {
-        console.log("Error in estimateFee: " + err);
-    }
-}
-async function start (chain, mnemonicOrKey, recipients, transferAmount) {
+async function start(chain, mnemonicOrKey, recipients, transferAmount) {
     try {
         const rpcEndpoint = chain.rpc;
-        let wallet, client;
-        let isMnemonic = validateMnemonic(mnemonicOrKey);
-        if (chain.slip44 && chain.slip44 === 60) {
-            if (isMnemonic) {
-                const privateKeyFromMnemonic = PrivateKey.fromMnemonic(mnemonicOrKey)
-                wallet = (await InjectiveDirectEthSecp256k1Wallet.fromKey(
-                    Buffer.from(privateKeyFromMnemonic.toPrivateKeyHex().replace("0x", ""), "hex"), chain.prefix))
-            } else {
-                wallet = (await InjectiveDirectEthSecp256k1Wallet.fromKey(
-                    Buffer.from(mnemonicOrKey, "hex"), chain.prefix));
-
-            }
-            client = new EthermintSigningClient(chain, wallet);
-        } else {
-            wallet = await Secp256k1HdWallet.fromMnemonic(
-                mnemonicOrKey,
-                { prefix: chain.prefix }
-            );
-            client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet);
-        }
+        let wallet = await getSigner(chain, mnemonicOrKey);
+        let client = new EthermintSigningClient(chain, wallet);
         const [account] = await wallet.getAccounts();
         const queryClient = await getQueryClient(rpcEndpoint);
         let balances = await queryClient.bank.balance(account.address, chain.denom);
